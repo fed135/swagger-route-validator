8d0a0ea9a51ea3843244c4f44f0720dd732fe872
Frederic Charette <fredericcharette@gmail.com>
2025-05-14 21:50:03 -0400

    added response header validation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,21 @@
 import {validateRequest} from './request';
 import {validateResponse} from './response';
+import {validateValue} from './valueValidator';
+
+function expressError(message, statusCode, title) {
+    this.message = message;
+    this.statusCode = statusCode;
+    this.title = title;
+}
 
 export function expressRequestValidation(routeSpec, spec?) {
     return function SRVRequestValidation(req, res, next) {
         const errors = validateRequest(routeSpec, req, spec);
-        if (errors.length > 0) throw new Error(`Request object does not match the specification for this route: ${JSON.stringify(errors)}`);
-
+        if (errors.length > 0) {
+            const errorObj = new expressError(`Request object does not match the specification for this route: ${JSON.stringify(errors)}`, 400, 'Bad Request');
+            errorObj.prototype = Error.prototype;
+            throw errorObj;
+        }
         next();
     }
 }
@@ -24,18 +34,34 @@ export function expressResponseValidation(routeSpec, options?: ResponseValidatio
         var oldSend = res.send;
 
         res.send = function(data) {
-            // Check status code
-            if (!routeSpec.responses[res.statusCode] && !routeSpec.responses['default']) {
-                throw new Error(`Response status code "${res.statusCode}" not present in spec for this route and no default responses could be found.`);
-            }
+            if (!res._validated) {
+                res._validated = true;
 
-            const responseObj = routeSpec.responses[res.statusCode] || routeSpec.responses['default'];
+                // Check status code
+                if (!routeSpec.responses[res.statusCode] && !routeSpec.responses['default']) {
+                    throw new Error(`Response status code "${res.statusCode}" not present in spec for this route and no default responses could be found.`);
+                }
 
-            const errors = validateResponse(responseObj.content ? responseObj.content['application/json'] : responseObj, JSON.parse(data), spec);
-            if (errors.length > 0) {
-                const errorMessage = `Response object does not match the specification for this route: ${JSON.stringify(errors)}`;
-                if (options.behavior === 'warn') console.log(errorMessage);
-                else throw new Error(errorMessage);
+                const responseObj = routeSpec.responses[res.statusCode] || routeSpec.responses['default'];
+
+                // TODO: Handle errors, reccursions and non-json data
+
+                const errors = validateResponse(responseObj.content ? responseObj.content['application/json'] : responseObj, JSON.parse(data), spec);
+                if (responseObj.headers) {
+                    for (let h in responseObj.headers) {
+                        validateValue(`header.${h}`, res.get(h), { name: h, ...responseObj.headers[h]}, () => {}, errors);
+                    }
+                }
+
+                if (errors.length > 0) {
+                    const errorMessage = `Response body does not match the specification for this route: ${JSON.stringify(errors)}`;
+                    if (options.behavior === 'warn') console.log(errorMessage);
+                    else {
+                        const errorObj = new expressError(errorMessage, 422, 'Unprocessable Content');
+                        errorObj.prototype = Error.prototype;
+                        throw errorObj;
+                    }
+                }
             }
 
             oldSend.apply(res, arguments);
--- tests/integration/middleware.spec.ts
+++ tests/integration/middleware.spec.ts
@@ -6,6 +6,10 @@ describe('Express app', () => {
     let server;
     let app;
     const port = 10000 + Math.round(Math.random() * 10000);
+    const jsonErrorHandler = (err, req, res, next) => {
+        res.status(err.statusCode || 500).send({ error: err.message });
+    }
+
     const spec = {
         definitions: {
             user: {
@@ -18,18 +22,29 @@ describe('Express app', () => {
             },
         },
         paths: {
-            '/foo': {
+            '/foo/{id}': {
+                parameters: [
+                    {
+                        name: 'authorization',
+                        in: 'header',
+                        required: true,
+                        type: 'string'
+                    }
+                ],
                 get: {
                     description: 'Get a User by Id',
                     parameters: [
                     {
                         name: 'id',
                         in: 'path',
-                        type: 'string',
+                        type: 'integer',
                     },
                     ],
                     responses: {
-                        default: { schema: { $ref: '#/definitions/user' } },
+                        default: {
+                            schema: { $ref: '#/definitions/user' },
+                            headers: { 'x-request-id': { type: 'string' } }
+                        },
                     },
                 },
             },
@@ -46,10 +61,12 @@ describe('Express app', () => {
   
     describe('with a request validation', () => {
       beforeEach((done) => {
-        server.get('/foo/:id', expressRequestValidation(spec.paths['/foo'].get, spec), (req, res, next) => {
+        server.get('/foo/:id', expressRequestValidation(spec.paths['/foo/{id}'].get, spec), (req, res, next) => {
           res.status(200).json({ id: req.params.id, name: 'John Smith', age: 99 });
           return next();
         });
+
+        server.use(jsonErrorHandler);
   
         app = server.listen(port, (err) => {
           if (err) throw err;
@@ -61,20 +78,38 @@ describe('Express app', () => {
         const {
           statusCode,
           body,
-        } = await request(`http://localhost:${port}/foo/123`);
+        } = await request(`http://localhost:${port}/foo/123`, { headers: {
+            authorization: '1234567890',
+        }});
         const response = await body.json();
   
         expect(statusCode).toEqual(200);
         expect(response).toEqual({ id: '123', name: 'John Smith', age: 99 });
       });
+
+      test('should reply with a 400 when sending invalid path parameters', async () => {
+        const {
+          statusCode,
+          body,
+        } = await request(`http://localhost:${port}/foo/abc`, { headers: {
+            authorization: '1234567890',
+        }});
+        const response = await body.json();
+  
+        expect(statusCode).toEqual(400);
+        expect(response).toEqual({ error: 'Request object does not match the specification for this route: [{\"error\":\"Value is not an integer\",\"cursor\":\"path.id\"}]' });
+      });
     });
 
-    describe('with a response validation', () => {
+    describe('with a response validation and response is well structured', () => {
         beforeEach((done) => {
-          server.get('/foo/:id', expressResponseValidation(spec.paths['/foo'].get, {behavior: 'error'}, spec), (req, res, next) => {
+          server.get('/foo/:id', expressResponseValidation(spec.paths['/foo/{id}'].get, {behavior: 'error'}, spec), (req, res, next) => {
+            res.setHeader('x-request-id', 'bar');
             res.status(200).json({ id: req.params.id, name: 'John Smith', age: 99 });
             return next();
           });
+
+          server.use(jsonErrorHandler);
     
           app = server.listen(port, (err) => {
             if (err) throw err;
@@ -86,12 +121,43 @@ describe('Express app', () => {
           const {
             statusCode,
             body,
-          } = await request(`http://localhost:${port}/foo/123`);
-
+          } = await request(`http://localhost:${port}/foo/123`, { headers: {
+            authorization: '1234567890',
+        }});
           const response = await body.json();
     
           expect(statusCode).toEqual(200);
           expect(response).toEqual({ id: '123', name: 'John Smith', age: 99 });
+        });
+      });
+
+      describe('with a response validation and response is invalid', () => {
+        beforeEach((done) => {
+          server.get('/foo/:id', expressResponseValidation(spec.paths['/foo/{id}'].get, {behavior: 'error'}, spec), (req, res, next) => {
+            res.status(200).json({ id: req.params.id, name: 'John Smith', age: 99 });
+            return next();
+          });
+
+          server.use(jsonErrorHandler);
+    
+          app = server.listen(port, (err) => {
+            if (err) throw err;
+            done();
+          });
+        });
+    
+        test('should reply with a 200 when sending valid path parameters', async () => {
+          const {
+            statusCode,
+            body,
+          } = await request(`http://localhost:${port}/foo/123`, { headers: {
+            authorization: '1234567890',
+        }});
+
+          const response = await body.json();
+    
+          expect(statusCode).toEqual(422);
+          expect(response).toEqual({ error: 'Response body does not match the specification for this route: [{\"error\":\"Value for x-request-id is required and was not provided\",\"cursor\":\"header.x-request-id\"}]' });
         });
       });
 });
